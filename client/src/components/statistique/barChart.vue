0c6642c708124e7d84a820869b2e367f98ead307
Lucas Roig <lucas1.roig@gmail.com>
2017-05-14 18:32:32 +0200

    Ajout des graphiques qui fonctionnent pas

---
+++ client/src/components/statistique/barChart.vue
@@ -0,0 +1,30 @@
+<template>
+    <canvas>{{dataObject}}</canvas>
+</template>
+<script>
+    import Chart from 'chart.js'
+export default {
+    props:['dataObject','options'],
+    name:'bar-chart',
+    data () {
+        return {
+            
+        }
+    },
+    methods:{
+        renderChart(){
+            this.myChart = new Chart(this.$el, {
+                type: 'bar',
+                data: this.dataObject,
+                options: this.options
+            });
+        },
+    },
+    mounted(){
+        this.renderChart();
+    },
+    updated(){
+        this.myChart.update();
+    }
+}
+</script>
